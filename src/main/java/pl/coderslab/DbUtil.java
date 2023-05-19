2b7e8db966b9feee7a7425c33bf8b5d137ee482a
Łukasz N <lnawrocki84@gmail.com>
2023-05-19 21:02:01 +0200

    19.05.2023

---
+++ src/main/java/pl/coderslab/DbUtil.java
@@ -0,0 +1,17 @@
+package pl.coderslab;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbUtil {
+
+    public static final String DB_URL = "jdbc:mysql://192.168.200.202:3306/workshop2?serverTimezone=UTC";
+    public static final String DB_USER = "lukasz";
+    public static final String DB_PASS = "Coderslab@0!3";
+
+    public static Connection connectProduct() throws SQLException {
+        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+    };
+
+}
--- src/main/java/pl/coderslab/Main.java
+++ src/main/java/pl/coderslab/Main.java
@@ -1,8 +1,11 @@
 package pl.coderslab;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
 
-        //System.out.println("Hello world!");
+
+
     }
-}
